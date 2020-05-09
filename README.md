52e830a0497a77f1d5fbb445adc06110cd5aac94
a-cs <acarneirosousa@gmail.com>
2020-05-08 23:59:53 -0300

    adicionando paginação

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Para ir além...
 
 Pensando em uma melhor experiência, você ainda pode adicionar outras funcionalidades. Aqui vão alguns exemplos:
 
-- [ ] **Limite de itens por página** - Paginar o resultado da API para que limite o número de items por página para não sobrecarregar o navegador, aumentando assim a performance do site para os usuários;
+- [x] **Limite de itens por página** - Paginar o resultado da API para que limite o número de items por página para não sobrecarregar o navegador, aumentando assim a performance do site para os usuários;
 - [ ] **Filtro de localização** - criar um filtro que exiba apenas os locais baseados na localização que o usuário configurar na busca - vocês precisarão criar uma nova base de dados com localização, desafiem-se!
 - [ ] **API do Google Maps** - Utilizando a API do Google Maps, é possível incluir pins onde o imóvel fica localizado no mapa, deixando assim bem mais fácil a visualização geográfica;
 - [ ] **Valor total da estadia** - a partir do input de check-in e check-out de usuários, o sistema pode calcular o número de estadias e mostrar uma prévia do valor total de hospedagem.
--- test.js
+++ test.js
@@ -1,35 +1,47 @@
 console.log("js carregado")
 var diasEstadia = 3
+const rowsPerPage = 5
+const app = document.getElementById('root')
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container-fluid')
+
+app.appendChild(container)
 
 
 
-async function readData(){
-    
+async function readData() {
+
     const read = await fetch("https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72")
     const data = await read.json();
-    
-    // document.getElementById('demo').innerHTML = data[x]["name"] ;
-    // document.getElementById('demo1').innerHTML = data[x]["property_type"] ;
-    // document.getElementById('demo2').innerHTML = "R$ "+ data[x]["price"] ;
-    // document.getElementById('imdemo').src= data[x]["photo"] ;
-    
-    const app = document.getElementById('root')
-    
-    const container = document.createElement('div')
-    container.setAttribute('class','container-fluid')
-    
-    app.appendChild(container)
+     
+    showData(data, rowsPerPage, 1)
+}
 
-    data.forEach(room => {
+
+function showData(data, rowsPerPage, page) {
+    
+    container.innerHTML=""
+
+    let start = rowsPerPage * (page - 1)
+    let end = start + rowsPerPage
+    
+    if (end>=data.length){
+        end=data.length   
+    }   
+     
+    for (i = start; i < end; i++) {
         
-        const card= document.createElement('div')
-        card.setAttribute('class','card')
+        let room = data[i]
+
+        const card = document.createElement('div')
+        card.setAttribute('class', 'card')
         card.setAttribute('style', 'max-width: 900px;')
-        card.innerHTML=
-        `<div class="row">
+        card.innerHTML =
+            `<div class="row">
             <div class="col-auto"><img
-                    src=${room.photo}
-                    class="card-img" style="max-width: 300px;"></div>
+                    src=${room.photo.replace(/=(.*)/g, "=medium")}
+                    class="card-img" ></div>
             <div class="col">
                 <div class="card-body">
                     <h1 class="card-title">${room.name}</h1>
@@ -40,11 +52,80 @@ async function readData(){
             </div>
         </div>`
 
-        
+
 
         container.appendChild(card)
-       
+
+    }
+    const pags = document.createElement('div')
+    pags.innerHTML = ""
+    container.appendChild(pags)
+    pagination(data, page, pags)
+}
+
+function pagination(data, page, wrapper) {
+
+    wrapper.innerHTML = ""
+
+
+    let numberOfPages = Math.ceil(data.length / rowsPerPage)
+
+    let myObj = ""
+    for (i = 1; i <= numberOfPages; i++) {
+        if (i == page) {
+            myObj += `<li class="page-item active"><a class="page-link" href="#" id="page${i}">${i}</a></li>`
+        } else {
+            myObj += `<li class="page-item"><a class="page-link" href="#" id="page${i}">${i}</a></li>`
+        }
+    }
+    let previousItem = ""
+    if (page == 1) {
+        previousItem = " disabled"
+    } else {
+        previousItem = ""
+    }
+    let nextItem = ""
+    if (page == numberOfPages) {
+        nextItem = " disabled"
+    } else {
+        nextItem = ""
+    }
+
+    wrapper.innerHTML =
+        `<nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item${previousItem}">
+        <a class="page-link" href="#" aria-label="Previous" id="previousId">
+          <span aria-hidden="true">&LT;</span>
+          <span class="sr-only">Previous</span>
+        </a>
+      </li>
+      ${myObj}
+      <li class="page-item${nextItem}">
+        <a class="page-link" href="#" aria-label="Next" id="nextId">
+          <span aria-hidden="true">&GT;</span>
+          <span class="sr-only">Next</span>
+        </a>
+      </li>
+    </ul>
+  </nav> `
+
+    document.getElementById("previousId").addEventListener("click", function () {
+    showData(data, rowsPerPage, (page-1))
+    })
+
+    
+    document.getElementById("nextId").addEventListener("click", function () {
+        showData(data, rowsPerPage, (page+1))
     })
     
-    
+
+    for (let i = 1; i <= numberOfPages; i++) {
+
+
+        document.getElementById(`page${i}`).addEventListener("click", function () {
+            showData(data, rowsPerPage, i)
+        })
+    }
 }
+
